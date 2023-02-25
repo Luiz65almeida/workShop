81e663e4a8dec4cdeafaf695e0e6a3f9b1393073
Luiz Henrique Almeida <luizhenrique65almeida@gmail.com>
2023-02-25 15:00:28 -0300

    Subtotal & Total methods

--- src/main/java/com/example/workShop/entities/OrderItem.java
+++ src/main/java/com/example/workShop/entities/OrderItem.java
@@ -66,6 +66,11 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    public Double getSubTotal(){
+        return price * quantity;
+    }
+
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -74,6 +79,7 @@ public class OrderItem implements Serializable {
         return result;
     }
 
+ 
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
